561383c03f0444c59d3bc7cf202937e15845fc61
Nikhil Wankhede <nikhil@kredx.com>
2018-07-17 08:13:59 +0530

    Packaged with setup.py

---
+++ lib/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='document-extraction',  # This is the name of your PyPI-package.
+    version='0.1',  # Update the version number for new releases
+    packages=find_packages()  # find all packages to be exported
+)
--- src/webapp/document_extract/document_extract_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DocumentExtractAppConfig(AppConfig):
-    name = 'document_extract_app'
